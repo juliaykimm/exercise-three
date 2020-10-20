1daa19ccde999bba1d808032e1ffaf4afeba585e
juliaykimm <54916475+juliaykimm@users.noreply.github.com>
2020-10-20 15:46:07 -0400

    added links

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const port = 4000; // port 3000 is what create react app uses,
 //so we can run this and react at the same time
 
 const indexRoute = require("./routes/index.js");
-const aboutRoute = require("./routes/index.js");
+const aboutRoute = require("./routes/about.js");
 /*---serve static files---*/
 app.use("/static", express.static("public")); // allows you to see cat.png
 //when you add "/static/cat.png" to the link
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  // want to send the request to the user
+  // want to send the response to the requestor
   res.send("Home Page: Exercise Three");
 });
 
